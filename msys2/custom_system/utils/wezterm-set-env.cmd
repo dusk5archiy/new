b02db8dd0b0a233b24ca8514b0491b43ceff46c0
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-15 21:14:43 +0700

    modify the path to .wezterm.lua

--- msys2/custom_system/utils/wezterm-set-env.cmd
+++ msys2/custom_system/utils/wezterm-set-env.cmd
@@ -1,4 +1,7 @@
 @echo off
-setx WEZTERM_CONFIG_FILE "%CWD%\wezterm\.wezterm.lua" /M
+set CWD=%~dp0
+set CWD=%CWD:~0,-1%
+for %%i in ("%CWD%\..\..\settings\config\wezterm\wezterm.lua") do set "p=%%~fi"
+setx WEZTERM_CONFIG_FILE "%p%" /M
 pause
 
