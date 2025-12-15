f8583cdc2af15024bfb779e8d8b4674b2420e151
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-15 16:25:24 +0000

    add 755 mode

--- archlinux/custom_system/system/main.sh
+++ archlinux/custom_system/system/main.sh
@@ -1,9 +1,9 @@
 # -----------------------------------------------------------------------------
 
-export $SYSTEM_DIR="$HOME/custom_system/system"
-export $STORE_DIR="$HOME/custom_system/store"
-export $VAR_SETTINGS_DIR="$HOME/var_settings"
-export $SETTINGS_DIR="$HOME/settings"
+export SYSTEM_DIR="$HOME/custom_system/system"
+export STORE_DIR="$HOME/custom_system/store"
+export VAR_SETTINGS_DIR="$HOME/var_settings"
+export SETTINGS_DIR="$HOME/settings"
 
 # -----------------------------------------------------------------------------
 
