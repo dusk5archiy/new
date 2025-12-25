4a32d2451e2d90cbdb036625753aab81b186c3ed
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-25 02:46:26 -0800

    add sudo to mkdir

--- archlinux/custom_system/system/scripts/mount.shared.sh
+++ archlinux/custom_system/system/scripts/mount.shared.sh
@@ -4,5 +4,5 @@ read -s -p 'Enter password: ' password_
 MOUNT_FOLDER='\mnt\shared'
 SHARED_ADDRESS="\\\\$(ip route show default | awk '/default/ {print $3}')\\shared"
 
-mkdir -p "$MOUNT_FOLDER"
+sudo mkdir -p "$MOUNT_FOLDER"
 sudo mount -t cifs $SHARED_ADDRESS $MOUNT_FOLDER -o user=$(echo $username_),password=$(echo $password_),uid=$(id -u),gid=$(id -g),forceuid,forcegid,file_mode=0777,dir_mode=0777
