a260c439ac7eb93f6b058ce685fbcfac0f656cf2
Dusk Sarchiy <dusqdn.eml@gmail.com>
2025-12-25 02:42:58 -0800

    change shared address

--- archlinux/custom_system/system/scripts/mount.shared.sh
+++ archlinux/custom_system/system/scripts/mount.shared.sh
@@ -2,7 +2,7 @@ read -p 'Enter username: ' username_
 read -s -p 'Enter password: ' password_
 
 MOUNT_FOLDER='\mnt\shared'
-SHARED_ADDRESS="\\$(ip route show default | awk '/default/ {print $3}')\shared"
+SHARED_ADDRESS="\\\\$(ip route show default | awk '/default/ {print $3}')\\shared"
 
 mkdir -p "$MOUNT_FOLDER"
 sudo mount -t cifs $SHARED_ADDRESS $MOUNT_FOLDER -o user=$(echo $username_),password=$(echo $password_),uid=$(id -u),gid=$(id -g),forceuid,forcegid,file_mode=0777,dir_mode=0777
