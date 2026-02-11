e06e0e3c1148b00c8f5078862cc1fd58a8345ecd
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-11 14:29:59 +0700

    fix snap

--- archlinux/s7sys/store/assets/ubuntu.nvim.sh
+++ archlinux/s7sys/store/assets/ubuntu.nvim.sh
@@ -1,2 +1,4 @@
 sudo apt install --no-install-recommends --no-install-suggests -y \
-  7zip fd-find fzf gcc git lazygit neovim python3 python3-venv ripgrep tree-sitter unzip npm sudo snap install neovim nodejs
+  7zip fd-find fzf gcc git lazygit neovim python3 python3-venv ripgrep tree-sitter unzip npm
+
+sudo snap install neovim nodejs
