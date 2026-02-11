106f037bdfdb8eac4ec7d2dd73165e426ed1cc68
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-11 14:36:56 +0700

    add update scripts

--- archlinux/s7sys/store/ins.default.sh
+++ archlinux/s7sys/store/ins.default.sh
@@ -1,3 +1,11 @@
+(
+  . /etc/os-release
+  filename="$STORE_DIR/assets/$ID._update.sh"
+  if [ -f "$filename" ]; then
+    . $filename
+  fi
+)
+
 apps=("git" "ssh" "nvim" "yazi")
 
 for app in "${apps[@]}"; do
