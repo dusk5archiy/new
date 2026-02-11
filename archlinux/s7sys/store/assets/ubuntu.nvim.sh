4256361cc10d1d0ee18ba13a7558faaebbe74cc7
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-02-11 20:13:15 +0700

    change tree-sitter-cli from apt to npm

--- archlinux/s7sys/store/assets/ubuntu.nvim.sh
+++ archlinux/s7sys/store/assets/ubuntu.nvim.sh
@@ -1,5 +1,8 @@
 sudo apt install --no-install-recommends --no-install-suggests -y \
-  fd-find fzf gcc git python3 python3-venv ripgrep unzip npm tree-sitter-cli
+  fd-find fzf gcc git python3 python3-venv ripgrep unzip npm
 
 sudo snap install --classic nvim
 sudo snap install --classic node
+
+sudo npm cache clean --force
+sudo npm install -g tree-sitter-cli
